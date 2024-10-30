93cb39ce3731079054f62b4fd8dd590e032fd39d
Juliano Augusto Da Silva Ambrosio <gestaojuliano99@gmail.com>
2024-10-30 16:57:36 -0300

    exercicio de construtores

---
+++ Exercicio-1/src/entities/Account.java
@@ -0,0 +1,62 @@
+package entities;
+
+public class Account {
+
+    //metodos privados
+    private int number;
+    private String holder;
+    private double balance;
+
+
+
+
+
+    //construtor de 1 argumento
+    public Account(int number, String holder) {
+        this.number = number;
+        this.holder = holder;
+    }
+
+    //contrtor de 3 argumentos
+    public Account(int number, String holder, double initialDeposit) {
+        this.number = number;
+        this.holder = holder;
+        deposit(initialDeposit);
+    }
+
+
+    //encapsulamento dos metodos.
+    public int getNumber() {
+        return number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
+        this.holder = holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    //criando a regra de deposito
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    //metodo para sacar + taxa de saque
+    public void withdraw(double amount){
+        balance -= amount + 5.0;
+    }
+
+    public String toString(){
+        return "Account "
+                + number
+                + ", Holder"
+                + ", Balance: $"
+                + String.format("%.2f", balance);
+    }
+}
